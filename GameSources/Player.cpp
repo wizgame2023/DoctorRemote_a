b73461ff66b94325f3bdfe7a0c4244d44dc9654a
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-03-28 09:53:23 +0900

    コンストラクタとoverrideした関数を作った

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -1,14 +1,29 @@
 /*!
 @file Player.cpp
-@brief プレイヤーなど実体
+@brief プレイヤー実体
+担当：逸見
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
-namespace basecross{
-	//test
+namespace basecross {
+	//--------------------------------------------------------------------------------------
+	//	class Player : public GameObject;
+	//	用途: プレイヤー
+	//--------------------------------------------------------------------------------------
+	Player::Player(const shared_ptr<Stage>& StagePtr) :
+		GameObject(StagePtr)
+	{}
+
+	Vec2 Player::GetInputState()const {
+	}
+
+	void Player::OnCreate(){
+
+	}
+	void Player::OnUpdate(){
+
+	}
 }
 //end basecross
-
-// test
