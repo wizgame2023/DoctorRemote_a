67c14c5a21de27c7a2fd6a4e9fa779bd760be5ce
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-03-28 09:41:02 +0900

    Player.hでclassを作り、OnCreate()とOnUpdate()を定義した

--- GameSources/Player.h
+++ GameSources/Player.h
@@ -1,13 +1,25 @@
 /*!
 @file Player.h
-@brief ƒLƒƒƒ‰ƒNƒ^[‚È‚Ç
+@brief プレイヤー
+担当：逸見
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
-	//test
+	//--------------------------------------------------------------------------------------
+	///	プレイヤー
+	//--------------------------------------------------------------------------------------
+	class Player : public GameObject {
+	public:
+		//コンストラクタ
+		Player(const shared_ptr<Stage>& StagePtr);
+		//デストラクタ
+		virtual ~Player() {}
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+	};
 }
-//end basecross
+//end namespace basecross
 
