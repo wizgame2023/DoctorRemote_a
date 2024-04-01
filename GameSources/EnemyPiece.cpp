656d518afe5670c9c906198b547a1066e2dbc860
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-04-01 18:41:58 +0900

    敵の欠片の描画処理を付け足した

--- GameSources/EnemyPiece.cpp
+++ GameSources/EnemyPiece.cpp
@@ -1,11 +1,46 @@
 /*!
-@file Character.cpp
-@brief キャラクターなど実体
+@file EnemyPiece.cpp
+@brief 敵の欠片実体
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+
+	EnemyPiece::EnemyPiece(const shared_ptr<Stage>& StagePtr,
+		const Vec3& scale,
+		const Quat& qt,
+		const Vec3& position
+	) :
+		GameObject(StagePtr),
+		m_scale(scale),
+		m_qt(qt),
+		m_position(position),
+		m_meshResName(L"DEFAULT_CUBE")
+
+	{}
+	EnemyPiece::~EnemyPiece() {};
+
+	void EnemyPiece::OnCreate() {
+		auto ptrTrans = GetComponent<Transform>();
+		ptrTrans->SetScale(m_scale);
+		ptrTrans->SetQuaternion(m_qt);
+		ptrTrans->SetPosition(m_position);
+
+		//オブジェクトの描画
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetMeshResource(m_meshResName);
+		ptrDraw->SetOwnShadowActive(true);
+		//ptrDraw->SetTextureResource(L"");
+		
+		//オブジェクトの影の描画
+		auto ptrShadow = AddComponent<Shadowmap>();
+		ptrShadow->SetMeshResource(m_meshResName);
+
+
+		AddTag(L"EnemyPiece");
+	}
 }
 //end basecross
--- GameSources/EnemyPiece.h
+++ GameSources/EnemyPiece.h
@@ -1,11 +1,29 @@
 /*!
-@file GameStage.h
-@brief ゲームステージ
+@file EnemyPiece.h
+@brief 敵の欠片
+担当：逸見
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
+	class EnemyPiece :public GameObject {
+	private:
+		Vec3 m_scale;
+		Quat m_qt;
+		Vec3 m_position;
+		wstring m_meshResName;
+
+	public:
+		EnemyPiece(const shared_ptr<Stage>& StagePtr,
+			const Vec3& scale,
+			const Quat& qt,
+			const Vec3& position
+		);
+		virtual ~EnemyPiece();
+
+		virtual void OnCreate() override;
+	};
 }
 //end namespace basecross
