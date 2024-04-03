242369267ad666c9b98fcc25fb8ad3cd00774aed
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-04-03 12:05:29 +0900

    enemyの実装

--- GameSources/Enemy.cpp
+++ GameSources/Enemy.cpp
@@ -1,11 +1,33 @@
 /*!
 @file Character.cpp
-@brief キャラクターなど実体
+@brief 敵など実体
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+	Enemy::Enemy(const shared_ptr<Stage>& StagePtr) :
+		GameObject(StagePtr)
+	{
+	}
+	void Enemy::OnCreate()
+	{
+		auto ptr = GetComponent<Transform>();
+		ptr->SetPosition(0, 0, 0);
+		ptr->SetRotation(0, 0, 0);
+		ptr->SetScale(1, 1, 1);
+
+		AddTag(L"Enemy");
+
+		auto shadowPtr = AddComponent<Shadowmap>();
+		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetOwnShadowActive(true);
+
+	}
+
 }
 //end basecross
--- GameSources/Enemy.h
+++ GameSources/Enemy.h
@@ -4,8 +4,16 @@
 */
 
 #pragma once
-#include "stdafx.h"
+#include "stdafx.h" 
 
 namespace basecross {
+	class Enemy :public GameObject
+	{
+	public:
+		Enemy(const shared_ptr<Stage>& StaegePtr);
+		~Enemy() {}
+		virtual void OnCreate() override;
+		//void OnUpdate() override;
+	};
 }
 //end namespace basecross
