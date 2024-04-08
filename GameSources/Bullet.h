ad8875f41dabc680aad3446bd048e901c4e7812b
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-04-09 08:41:43 +0900

    弾の引数を増やしました

--- GameSources/Bullet.h
+++ GameSources/Bullet.h
@@ -1,5 +1,5 @@
 /*!
-@file GameStage.h
+@file Bullet.h
 @brief 弾
 担当　三瓶裕太
 */
@@ -11,20 +11,26 @@ namespace basecross {
 	class Bullet : public GameObject
 	{
 	private:
-		Vec3 m_Position;
-		Vec3 m_Scale;
-		float m_Speed;
+		Vec3 m_Position;//場所
+		Vec3 m_Scale;//サイズ
+		float m_Speed;//速さ
+		float m_angle;//角度
+		int m_Attack;//攻撃力
 	public:
 		Bullet(
 			   const shared_ptr<Stage>& StagePtr,
 			   const Vec3& Position,
 		       const Vec3& Scale,
-			   float Speed
+			   float Speed,
+			   float angle,
+			   int Attack
 		       );
 		~Bullet();
 		void OnCreate()override;//初期化UnityでいうところのStart
 		void OnUpdate()override;//アップデート
 		void OnCollisionEnter(shared_ptr<GameObject>& Other) override;//コリジョンが入った時
+		float GetSpeed();
+	
 	};
 }
 //end namespace basecross
