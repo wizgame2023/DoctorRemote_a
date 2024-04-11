d67ea17892907b93effcf3943ee67d79eba9bc67
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-04-11 11:32:58 +0900

    Enemyの死亡判定

--- GameSources/Enemy.cpp
+++ GameSources/Enemy.cpp
@@ -9,13 +9,13 @@
 
 namespace basecross {
 	Enemy::Enemy(const shared_ptr<Stage>& StagePtr) :
-		GameObject(StagePtr),m_Hp(3)
+		GameObject(StagePtr),m_Hp(1)
 	{
 	}
 	void Enemy::OnCreate()
 	{
 		auto ptr = GetComponent<Transform>();
-		ptr->SetPosition(0, 0, 0);
+		ptr->SetPosition(0, 0.5f, 0);
 		ptr->SetRotation(0, 0, 0);
 		ptr->SetScale(1, 1, 1);
 
@@ -33,6 +33,7 @@ namespace basecross {
 		GetStage()->SetCollisionPerformanceActive(true);
 		GetStage()->SetUpdatePerformanceActive(true);
 		GetStage()->SetDrawPerformanceActive(true);
+
 	}
 
 
@@ -43,16 +44,21 @@ namespace basecross {
 
 	void Enemy::OnCollisionEnter(shared_ptr<GameObject>& Collision)
 	{
+		m_bullet = dynamic_pointer_cast<Bullet>(Collision);
 		//Collision->GetStage()->SetSharedGameObject(L"Bullet", Bulletptr);
-		int attack = m_bullet.lock()->GetAttack();
+		if (!m_bullet.expired())
+		{
 
-		if (Collision->FindTag(L"Bullet") && m_Hp > 0)
-		{
-			m_Hp = m_Hp - attack;
-		}
-		if (m_Hp <= 0)
-		{
-			DestroyGameObject();
+			int attack = m_bullet.lock()->GetAttack();
+
+			if (Collision->FindTag(L"Bullet") && m_Hp > 0)
+			{
+				m_Hp = m_Hp - attack;
+			}
+			if (m_Hp <= 0)
+			{
+				GetStage()->RemoveGameObject<Enemy>(GetThis<Enemy>());
+			}
 		}
 	}
 
