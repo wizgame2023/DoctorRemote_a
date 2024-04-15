014ee5794f320a40f25088cd38fe861001d77096
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-04-15 15:45:38 +0900

    FakePiece描画処理を追加した

--- GameSources/FakePiece.h
+++ GameSources/FakePiece.h
@@ -1,11 +1,30 @@
 /*!
 @file GameStage.h
 @brief ゲームステージ
+担当：逸見
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
+	class FakePiece :public GameObject {
+
+	private:
+		Vec3 m_scale;
+		Quat m_qt;
+		Vec3 m_position;
+		wstring m_meshResName;
+
+	public:
+		FakePiece(const shared_ptr<Stage>& StagePtr,
+			const Vec3& scale,
+			const Quat& qt,
+			const Vec3& position);
+		virtual ~FakePiece(){}
+
+		virtual void OnCreate() override;
+		virtual void OnCollisionEnter(shared_ptr<GameObject>& other) override;
+	};
 }
 //end namespace basecross
