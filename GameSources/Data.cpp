78f8c7c1ed065008284739a23c0c12772b197995
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-04-16 15:34:59 +0900

    テクスチャなどのデータを管理するファイルを追加

---
+++ GameSources/Data.cpp
@@ -0,0 +1,11 @@
+/*!
+@file Data.cpp
+@brief テクスチャ、モデル
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+}
+//end basecross
---
+++ GameSources/Data.h
@@ -0,0 +1,11 @@
+/*!
+@file Data.h
+@brief テクスチャ、モデル
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+}
+//end namespace basecross
