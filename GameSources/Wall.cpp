01c7db73dc1aedb7a8ac7f0eb4029a4ce3a3abd1
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-04-17 13:11:16 +0900

    Wallの追加と欠片ゲージのテクスチャの変更

--- GameSources/Wall.cpp
+++ GameSources/Wall.cpp
@@ -1,11 +1,38 @@
 /*!
 @file BreakWall.cpp
-@brief ‰ó‚ê‚é•Ç
+@brief •Ç
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+	Wall::Wall(const shared_ptr<Stage>& StagePtr) :
+		GameObject(StagePtr)
+	{
+	}
+	
+	void Wall::OnCreate()
+	{
+		auto ptr = GetComponent<Transform>();
+		ptr->SetPosition(0.0f, 0.5f, 0.0f);
+		ptr->SetRotation(0, 0, 0);
+		ptr->SetScale(1, 1, 1);
+
+		auto shadowPtr = AddComponent<Shadowmap>();
+		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		ptrDraw->SetFogEnabled(true);
+		ptrDraw->SetOwnShadowActive(true);
+
+		auto ptrColl = AddComponent<CollisionSphere>();
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+	}
+
 }
 //end namespace basecross
--- GameSources/Wall.h
+++ GameSources/Wall.h
@@ -1,12 +1,23 @@
 /*!
 @file BreakWall.h
-@brief ‰ó‚ê‚é•Ç
+@brief •Ç
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
+	class Wall : public GameObject
+	{
+	public:
+		Wall(const shared_ptr<Stage>& StagePtr);
+		~Wall() {}
+		virtual void OnCreate() override; // ‰Šú‰»
+		//virtual void OnUpdate() override; // XV
+		//virtual void OnDraw() override; // •`‰æ
+
+	};
+
 }
 //end namespace basecross
 
