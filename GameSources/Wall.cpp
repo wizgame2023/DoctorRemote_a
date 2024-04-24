820af646be4174668a90c1bca1a3a2876ffbd8aa
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-04-24 09:39:06 +0900

    壁の配置とWall.cppとWall.h中身の変更

--- GameSources/Wall.cpp
+++ GameSources/Wall.cpp
@@ -7,32 +7,26 @@
 #include "Project.h"
 
 namespace basecross {
-	Wall::Wall(const shared_ptr<Stage>& StagePtr) :
-		GameObject(StagePtr)
+	Wall::Wall(const shared_ptr<Stage>& StagePtr, const Vec3& pos, const Vec3& rot, const Vec3& scale) :
+
+		GameObject(StagePtr),m_pos(pos),m_rot(rot),m_scale(scale)
 	{
 	}
-	
+
 	void Wall::OnCreate()
 	{
 		auto ptr = GetComponent<Transform>();
-		ptr->SetPosition(0.0f, 0.5f, 0.0f);
-		ptr->SetRotation(0, 0, 0);
-		ptr->SetScale(151.5, 10, 0.5);
+		ptr->SetPosition(m_pos);
+		ptr->SetRotation(m_rot);
+		ptr->SetScale(m_scale);
 
-		auto shadowPtr = AddComponent<Shadowmap>();
-		shadowPtr->SetMeshResource(L"DEFAULT_CUBE");
 		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
 		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
 		ptrDraw->SetFogEnabled(true);
-		ptrDraw->SetOwnShadowActive(true);
-
-		auto ptrColl = AddComponent<CollisionSphere>();
 
 		GetStage()->SetCollisionPerformanceActive(true);
 		GetStage()->SetUpdatePerformanceActive(true);
 		GetStage()->SetDrawPerformanceActive(true);
-
 	}
-
 }
 //end namespace basecross
