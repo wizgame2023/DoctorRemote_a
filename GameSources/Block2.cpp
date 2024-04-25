9d9c8eae5425a973bd098c3c5d6e0692cdfccafd
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-04-25 11:05:45 +0900

    Block1の実体追加・配置

--- GameSources/Block2.cpp
+++ GameSources/Block2.cpp
@@ -7,5 +7,24 @@
 #include "Project.h"
 
 namespace basecross {
+	BlockSecond::BlockSecond(const shared_ptr<Stage>& StagePtr, const Vec3& pos, const Vec3& rot, const Vec3& scale) :
+		GameObject(StagePtr), m_pos(pos), m_rot(rot), m_scale(scale)
+	{
+	}
+	void BlockSecond::OnCreate()
+	{
+		auto ptr = GetComponent<Transform>();
+		ptr->SetPosition(m_pos);
+		ptr->SetRotation(m_rot);
+		ptr->SetScale(m_scale);
+
+		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
+		ptrDraw->SetMeshResource(L"DEFAULT_CYLINDER");
+		ptrDraw->SetFogEnabled(true);
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+	}
 }
 //end namespeca basecross
