3fb40bc2c5ffe4ba675b9e3f426c0f809442f6db
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-04-25 10:00:50 +0900

    デバック用ステージの更新

--- GameSources/SanpeiStage.cpp
+++ GameSources/SanpeiStage.cpp
@@ -180,10 +180,15 @@ namespace basecross {
 			//ゲージを追加
 			auto garge = AddGameObject<PieceGarge>(GetSharedGameObject<Player>(L"GamePlayer"));
 			SetSharedGameObject(L"Garge", garge);
+			auto PGarge = AddGameObject<PlayerGarge>();
 
 			//壁を出現（テスト）
-			AddGameObject<BreakWall>(Vec3(1.0f, 1.0f, 1.0f), Vec3(1.0f, 1.0f, 1.0f), Vec3(0.0f, 0.0f, 0.0f));
-			AddGameObject<RecoveryWall>(Vec3(15.0f, 1.0f, 10.0f), Vec3(2.0f, 2.0f, 2.0f), Vec3(0.0f, 0.0f, 0.0f));
+			AddGameObject<BreakWall>(Vec3(1.0f, 1.0f, 1.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(3.0f, 3.0f, 3.0f));
+			AddGameObject<RecoveryWall>(Vec3(15.0f, 1.0f, 10.0f), Vec3(2.0f, 2.0f, 2.0f), Vec3(2.0f, 2.0f, 2.0f));
+
+			auto stageManager = AddGameObject<StageManager>();//ステージマネージャー追加
+			SetSharedGameObject(L"StageManager", stageManager);
+
 
 			//エフェクトを出現（テスト）
 			//AddGameObject<EffectBullet>(L"DamageBullet",3, 2, Vec3(-1.0f, 0.5f, 0.0f), 0.1f);
