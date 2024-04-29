914813a960a01d2e98c743921a783650046e80a7
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-04-29 10:43:54 +0900

    Stageのファイルの追加と整頓

---
+++ GameSources/GameOverStage.cpp
@@ -0,0 +1,11 @@
+/*!
+@file GameOverStageStage.cpp
+@brief ゲームオーバーステージ実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+}
+//end basecross
---
+++ GameSources/GameOverStage.h
@@ -0,0 +1,12 @@
+/*!
+@file GameOverStage.h
+@brief ゲームオーバーステージ
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+}
+//end namespace basecross
+
