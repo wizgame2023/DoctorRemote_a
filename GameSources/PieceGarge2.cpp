841b834c9473c4be84b8ecc378250a3a430ad8de
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-01 11:12:14 +0900

    ゲージにFrameの追加

--- GameSources/PieceGarge2.cpp
+++ GameSources/PieceGarge2.cpp
@@ -32,6 +32,8 @@ namespace basecross {
 		Vec3 zero(0, 0, 0);
 
 		m_garge = stage->AddGameObject<Garge>(m_trans, 0, 1024, m_meshResName, m_ratio, 40, 240, color, color2, screen, dis);
+
+		auto frame = stage->AddGameObject<Sprite>(42, 242, L"BarFrame",Vec3(540, -220, 1.0f));
 	}
 
 	void PieceGarge2::OnUpdate() {
