f552fe07aef054d5d8738aca2765b969baa0594f
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-02 10:57:32 +0900

    デバックステージの更新

--- GameSources/SanpeiStage.cpp
+++ GameSources/SanpeiStage.cpp
@@ -186,6 +186,10 @@ namespace basecross {
 			AddGameObject<BreakWall>(Vec3(5.0f, 0.0f, 5.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(2.0f, 2.0f, 2.0f));
 			AddGameObject<RecoveryWall>(Vec3(15.0f, 0.0f, 10.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f));
 
+			AddGameObject<Block>(Vec3(10.0f, -0.5f,-2.0f), Vec3(0.0f, 0.0f, 0.0f));
+			AddGameObject<BlockSecond>(Vec3(-10.5f, 4.5f, 15.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(20.0f, 10.0f, 20.0f));
+
+
 			auto stageManager = AddGameObject<StageManager>();//ステージマネージャー追加
 			SetSharedGameObject(L"StageManager", stageManager);
 
