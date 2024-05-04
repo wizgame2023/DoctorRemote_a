b2aa74c99f18bd73bb00025ae24bc99fafe14f23
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-04 11:56:56 +0900

    PlayerとBulletの更新

--- GameSources/Bullet.cpp
+++ GameSources/Bullet.cpp
@@ -99,7 +99,7 @@ namespace basecross {
 		//‚à‚µ‚Ô‚Â‚©‚Á‚½ƒRƒŠƒWƒ‡ƒ“‚ªEnemy‚Ì‚à‚Ì‚¾‚Á‚½‚ç
 		if (Other->FindTag(L"Enemy"))
 		{			
-			GetStage()->AddGameObject<EffectBullet>(L"DamageBullet", 3, 2, GetComponent<Transform>()->GetPosition(), 0.1f);
+			//GetStage()->AddGameObject<EffectBullet>(L"DamageBullet", 3, 2, GetComponent<Transform>()->GetPosition(), 0.1f);
 			//DestroyGameObject();//Ž©•ª‚ÍÁ‚¦‚é
 			GetStage()->RemoveGameObject<Bullet>(GetThis<Bullet>());
 
