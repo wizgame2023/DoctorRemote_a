adb0d69e9524ea24d930f679b983bca00be13a51
Tarui Kosuke <zeal23300101@fsgcl.jp>
2024-05-07 15:52:17 +0900

    ロードの内容とテクスチャ追加

--- GameSources/LoadStage.h
+++ GameSources/LoadStage.h
@@ -7,4 +7,18 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class LoadStage :public Stage {
+
+		void CreateViewLight();
+
+	public:
+		LoadStage() :Stage() {}
+		virtual ~LoadStage() {}
+
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		void StageChange();
+	};
+
 }
