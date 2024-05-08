9872b638e2c31b0cafff18e199311a5106d43ba3
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-08 14:26:50 +0900

    Effectの作成

--- GameSources/Effect.cpp
+++ GameSources/Effect.cpp
@@ -1,11 +1,84 @@
 /*!
-@file Character.cpp
+@file effect.cpp
 @brief キャラクターなど実体
+担当　三瓶裕太
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+	Effect::Effect(shared_ptr<Stage>& StagePtr,wstring TextureName,Vec3 pushPos = Vec3(1.0f,1.0f,1.0f)) :
+		MultiParticle(StagePtr),
+		m_TextureName(TextureName),
+		m_pushPos(pushPos)
+	{
+
+	}
+
+	Effect::~Effect()
+	{
+
+	}
+
+	void Effect::OnCreate()
+	{
+
+	}
+
+	void Effect::OnUpdate()
+	{
+		
+		for (auto ptrParticle : GetParticleVec())
+		{
+			for (auto& rParticleSprite : ptrParticle->GetParticleSpriteVec())
+			{
+				if (rParticleSprite.m_Active)
+				{
+					//rParticleSprite.m_LocalScale = Vec3(0.1f, 0.1f, 0.1f);
+					//rParticleSprite.m_Color += 0.05f;
+					if (rParticleSprite.m_Color.z >= 1.0f)
+					{
+						rParticleSprite.m_Color.z = 1.0f;
+					}
+				}
+			}
+		}
+
+		//親クラスのOnUpdate()を呼ぶ
+		MultiParticle::OnUpdate();
+	}
+
+	void Effect::InsertEffect(const Vec3& Pos)
+	{
+		auto ptrParticle = InsertParticle(1);//何個同時にテクスチャが出てくるか決める
+		ptrParticle->SetEmitterPos(Pos);
+		ptrParticle->SetTextureResource(m_TextureName);
+		ptrParticle->SetMaxTime(1.5f);	
+		//srand(time(0));
+		float randam = rand() % 6 + 1;
+		randam -= 3;
+		float randam2 = rand() % 6 + 1;
+		randam2 -= 3;
+
+		Vec3 pushPos = Vec3(0.2f, 0.8f, 0.2f);
+
+		for (auto& rParticleSprite : ptrParticle->GetParticleSpriteVec())
+		{	
+
+			rParticleSprite.m_LocalPos.x = Util::RandZeroToOne()*randam;
+			rParticleSprite.m_LocalPos.y = Util::RandZeroToOne();
+			rParticleSprite.m_LocalPos.z = Util::RandZeroToOne()*randam2;
+			//各パーティクルの移動速度を指定
+			rParticleSprite.m_Velocity = Vec3
+			(
+				rParticleSprite.m_LocalPos.x * m_pushPos.x,
+				rParticleSprite.m_LocalPos.y * m_pushPos.y,
+				rParticleSprite.m_LocalPos.z * m_pushPos.z
+			);
+			rParticleSprite.m_Color = Col4(1.0f, 1.0f, 1.0f, 1.0f);
+
+		}
+	}
 }
 //end basecross
--- GameSources/Effect.h
+++ GameSources/Effect.h
@@ -1,11 +1,29 @@
 /*!
-@file GameStage.h
+@file Effect.h
 @brief ゲームステージ
+担当　三瓶裕太
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
+	class Effect :public MultiParticle
+	{
+	private:
+		wstring m_TextureName;
+		Vec3 m_pushPos;
+	public:
+		//コンストラクタ
+		explicit Effect(shared_ptr<Stage>& StagePtr,wstring TextureName,Vec3 pushPos);
+		//デストラクタ
+		virtual ~Effect();
+
+		//初期化
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		virtual void InsertEffect(const Vec3& Pos);
+	};
 }
 //end namespace basecross
