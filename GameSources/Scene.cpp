7baba3cd50ad2790a109f043603c519b46a4285a
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-08 15:02:15 +0900

    Sceneの修正

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -154,7 +154,7 @@ namespace basecross {
     void Scene::OnEvent(const shared_ptr<Event>& event) {
         if (event->m_MsgStr == L"ToGameStage") {
             //ゲームステージの設定
-			ResetActiveStage<SanpeiStage>();
+			ResetActiveStage<GameStage>();
 
         }
         else if (event->m_MsgStr == L"ToStartStage") {
