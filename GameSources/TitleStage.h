0c680ae01ea34a6661562d11267d30f6c93c229e
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-05-08 12:07:36 +0900

    taitelの中身追加・テクスチャの追加

--- GameSources/TitleStage.h
+++ GameSources/TitleStage.h
@@ -7,4 +7,16 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class TitleStage :public Stage
+	{
+		void CreateViewLight();
+	public:
+		TitleStage() :Stage(){}
+			virtual ~TitleStage() {}
+		virtual void OnCreate() override;
+		virtual void OnUpdate() override;
+
+		void StageChange();
+
+	};
 }
