e2c065d080e865442bb4cea04ff7d787312bed4c
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-10 11:44:04 +0900

    エフェクトの更新

--- GameSources/Effect.cpp
+++ GameSources/Effect.cpp
@@ -8,12 +8,13 @@
 #include "Project.h"
 
 namespace basecross {
-	Effect::Effect(shared_ptr<Stage>& StagePtr,wstring TextureName,Vec3 pushPos,Vec3 Scale) :
+	Effect::Effect(shared_ptr<Stage>& StagePtr,wstring TextureName,float maxTime,int addNumber,Vec3 pushPos,Vec3 Scale) :
 		MultiParticle(StagePtr),
 		m_TextureName(TextureName),
 		m_pushPos(pushPos),
 		m_Scale(Scale),
-		m_maxTime(0.5f)
+		m_maxTime(maxTime),
+		m_addNumber(addNumber)
 	{
 
 	}
@@ -53,22 +54,27 @@ namespace basecross {
 
 	void Effect::InsertEffect(const Vec3& Pos)
 	{
-		auto ptrParticle = InsertParticle(1);//何個同時にテクスチャが出てくるか決める
+		auto ptrParticle = InsertParticle(m_addNumber);//何個同時にテクスチャが出てくるか決める
 		ptrParticle->SetEmitterPos(Pos);
 		ptrParticle->SetTextureResource(m_TextureName);
 		ptrParticle->SetMaxTime(m_maxTime);
 		//srand(time(0));
-		float randamPosX = rand() % 6 + 1;
-		randamPosX -= 3;
-		float randamPosZ = rand() % 6 + 1;
-		randamPosZ -= 3;
+		float randamPosX = rand() % 30+10;
+		randamPosX -= 20;
+		randamPosX /= 10;//小数点を入れる
+		float randamPosZ = rand() % 30 + 10;
+		randamPosZ -= 20;
+		randamPosZ /= 10;//小数点を入れる
+		float randamPosY = rand() % 30 + 10;
+		randamPosY -= 20;
+		randamPosY /= 10;//小数点を入れる
 
 		for (auto& rParticleSprite : ptrParticle->GetParticleSpriteVec())
 		{	
 			//パーティクルの生成位置を決定
-			rParticleSprite.m_LocalPos.x = Util::RandZeroToOne()*randamPosX;
-			rParticleSprite.m_LocalPos.y = Util::RandZeroToOne();
-			rParticleSprite.m_LocalPos.z = Util::RandZeroToOne()*randamPosZ;
+			rParticleSprite.m_LocalPos.x = (Util::RandZeroToOne() * 2 - 0.5f) * randamPosX;
+			rParticleSprite.m_LocalPos.y = (Util::RandZeroToOne() * 2 - 0.5f) * randamPosY;
+			rParticleSprite.m_LocalPos.z = (Util::RandZeroToOne() * 2 - 0.5f) * randamPosZ;
 			//各パーティクルの移動速度を指定
 			rParticleSprite.m_Velocity = Vec3
 			(
