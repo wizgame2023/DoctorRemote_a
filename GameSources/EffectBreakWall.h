7c939c32194f2f36b9c000467ca18c85b22ea87a
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-13 14:39:59 +0900

    Effect関係の更新

--- GameSources/EffectBreakWall.h
+++ GameSources/EffectBreakWall.h
@@ -11,11 +11,12 @@ namespace basecross {
 	class EffectBreakWall : public Effect
 	{
 	private:
+		//int m_addNumber;
 	public:
 		EffectBreakWall(shared_ptr<Stage>& Stageptr, wstring TextureName, float maxTime, int addNumber, Vec3 pushPos = Vec3(1.0f, 1.0f, 1.0f), Vec3 Scale = Vec3(1.0f, 1.0f, 1.0f));
 		~EffectBreakWall() override;
 
-		//virtual void InsertEffect(const Vec3& Pos) override;
+		virtual void InsertEffect(const Vec3& Pos) override;
 
 	};
 }
