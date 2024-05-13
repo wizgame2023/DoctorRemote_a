dd579360affd955374b9050a2e0d8e4d3093e488
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-13 14:06:08 +0900

    EffectPlayerのファイルを追加

---
+++ GameSources/EffectPlayer.h
@@ -0,0 +1,11 @@
+/*!
+@file EffectBullet.h
+@brief 弾の当たった時のエフェクト
+担当：三瓶裕太
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+}
--- GameSources/Project.h
+++ GameSources/Project.h
@@ -42,6 +42,7 @@
 #include "Effect.h"
 #include "EffectBullet.h"
 #include "EffectBreakWall.h"
+#include "EffectPlayer.h"
 #include "Block.h"
 #include "Block2.h"
 #include "BreakWall.h"
