68c90081490fc8d0d0255f85ee0aef97a49ad46e
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-05-14 12:34:10 +0900

    timeの表示・Time基準のランクの設定

--- GameSources/ScoreStage.cpp
+++ GameSources/ScoreStage.cpp
@@ -27,7 +27,25 @@ namespace basecross {
 			AddGameObject<Sprite>(1280, 800, L"Score", Vec3());
 			auto rank= AddGameObject<Sprite>(512, 800, L"Rank", Vec3(350,50,0));
 			srand(time(0));
-			rank->UpdateRank(rand() % 4);
+			m_time = App::GetApp()->GetScene<Scene>()->GetTime();
+			AddGameObject<TimeManager>(false,m_time);
+
+			if (m_time > 50)
+			{
+				rank->UpdateRank(0);
+			}
+			if (m_time > 40 && m_time < 50)
+			{
+				rank->UpdateRank(1);
+			}
+			if (m_time > 25 && m_time < 40)
+			{
+				rank->UpdateRank(2);
+			}
+			if (m_time < 25)
+			{
+				rank->UpdateRank(3);
+			}
 		}
 		catch (...) {
 			throw;
@@ -36,16 +54,14 @@ namespace basecross {
 
 	void ScoreStage::OnUpdate() {
 		StageChange();
-		AddGameObject<TimeManager>(false);
-		App::GetApp()->GetScene<Scene>()->GetTime();
 	}
 	void ScoreStage::StageChange() {
 		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
 		if (cntlVec[0].bConnected)
 		{
-			if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A)
+			if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_B)
 			{
-				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToStartStage");
+				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToStatusStage");
 			}
 		}
 	}
