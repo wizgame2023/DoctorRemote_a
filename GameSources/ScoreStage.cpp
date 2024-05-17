84d8ebb9aa447e69b7ac39c1393b554aefdedf8c
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-05-17 14:02:07 +0900

    スコアステージの更新

--- GameSources/ScoreStage.cpp
+++ GameSources/ScoreStage.cpp
@@ -26,7 +26,6 @@ namespace basecross {
 
 			AddGameObject<Sprite>(1280, 800, L"Score", Vec3());
 			m_time = App::GetApp()->GetScene<Scene>()->GetTime();
-			AddGameObject<TimeManager>(false, m_time);
 		}
 		catch (...) {
 			throw;
@@ -37,60 +36,64 @@ namespace basecross {
 	{
 		float delta = App::GetApp()->GetElapsedTime();
 		m_countUp += 1*delta;
-
 		StageChange();
 		if (m_count == 0)
 		{
-			if (m_countUp >= 2.0f && m_timeCount == 0)
+			if (m_countUp >= 1.0f && m_timeCount == 0)
+			{
+				AddGameObject<TimeManager>(m_time, Vec3(450, 400, 0));
+				m_timeCount++;
+			}
+			else if (m_countUp >= 2.0f && m_timeCount == 1)
 			{
 				auto ifClear = AddGameObject<Sprite>(512, 75, L"IfClear", Vec3(-300, 100, 0));
 				ifClear->UpdateIfClear(0);
 				m_timeCount++;
 			}
-			if (m_time >= 25 && m_countUp >= 3.0f && m_timeCount == 1)
+			else if (m_time >= 25 && m_countUp >= 3.0f && m_timeCount == 2)
 			{
 				auto ifClear = AddGameObject<Sprite>(512, 75, L"IfClear", Vec3(-300, 10, 0));
 				ifClear->UpdateIfClear(1);
 				m_timeCount++;
 			}
-			if (m_time >= 40 && m_countUp >= 4.0f && m_timeCount == 2)
+			else if (m_time >= 40 && m_countUp >= 4.0f && m_timeCount == 3)
 			{
 				auto ifClear = AddGameObject<Sprite>(512, 75, L"IfClear", Vec3(-300, -80, 0));
 				ifClear->UpdateIfClear(2);
 				m_timeCount++;
 			}
-			if (m_time >= 50 && m_countUp >= 5.0f && m_timeCount == 3)
+			else if (m_time >= 50 && m_countUp >= 5.0f && m_timeCount == 4)
 			{
 				auto ifClear = AddGameObject<Sprite>(512, 75, L"IfClear", Vec3(-300, -170, 0));
 				ifClear->UpdateIfClear(3);
 				m_timeCount++;
 			}
 
-
-			if (m_time >= 50&& m_countUp >= 6.0f && m_timeCount == 4)
+			else if (m_time >= 50 && m_countUp >= 6.0f && m_timeCount == 5)
 			{
 				auto rank = AddGameObject<Sprite>(512, 800, L"Rank", Vec3(350, 50, 0));
 				rank->UpdateRank(0);
 				m_count++;
 			}
-			if (m_time >= 40 && m_time < 50 && m_countUp >= 6.0f && m_timeCount == 4)
+			else if (m_time >= 40 && m_time < 50 && m_countUp >= 6.0f && m_timeCount == 5)
 			{
 				auto rank = AddGameObject<Sprite>(512, 800, L"Rank", Vec3(350, 50, 0));
 				rank->UpdateRank(1);
 				m_count++;
 			}
-			if (m_time >= 25 && m_time < 40 && m_countUp >= 6.0f && m_timeCount == 4)
+			else if (m_time >= 25 && m_time < 40 && m_countUp >= 6.0f && m_timeCount == 5)
 			{
 				auto rank = AddGameObject<Sprite>(512, 800, L"Rank", Vec3(350, 50, 0));
 				rank->UpdateRank(2);
 				m_count++;
 			}
-			if (m_time < 25 && m_countUp >= 6.0f && m_timeCount == 4)
+			else if (m_time < 25 && m_countUp >= 6.0f && m_timeCount == 5)
 			{
 				auto rank = AddGameObject<Sprite>(512, 800, L"Rank", Vec3(350, 50, 0));
 				rank->UpdateRank(3);
 				m_count++;
 			}
+			m_moveCount = -400;
 
 		}
 		//wstringstream wss(L"");
