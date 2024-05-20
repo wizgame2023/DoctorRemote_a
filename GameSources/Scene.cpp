146dcb5185762c05fd5ac902023d43f6259bec8c
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-20 16:12:49 +0900

    テクスチャのパス追加

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -54,10 +54,8 @@ namespace basecross {
 		app->RegisterTexture(L"Ligth2", strTexture);
 		strTexture = texPath + L"Ligth3.png";
 		app->RegisterTexture(L"Ligth3", strTexture);
-		strTexture = texPath + L"gameover_2.jpg";
-		app->RegisterTexture(L"GameOver2", strTexture);
-		strTexture = texPath + L"taitel_2.jpg";
-		app->RegisterTexture(L"Title2", strTexture);
+		strTexture = texPath + L"Back.png";
+		app->RegisterTexture(L"Back", strTexture);
 
 		strTexture = texPath + L"RadarBar.png";
 		app->RegisterTexture(L"RadarBar", strTexture);
@@ -97,16 +95,21 @@ namespace basecross {
 		strTexture = texPath + L"EffectWhite.png";
 		app->RegisterTexture(L"PlayerEffectWhite", strTexture);
 
-		strTexture = texPath + L"DashButton.png";
+		strTexture = texPath + L"DashMode.png";
 		app->RegisterTexture(L"DashButton", strTexture);
-		strTexture = texPath + L"BulletButton.png";
+		strTexture = texPath + L"BulletMode.png";
 		app->RegisterTexture(L"BulletButton", strTexture);
 		strTexture = texPath + L"PieceButton.png";
 		app->RegisterTexture(L"PieceButton", strTexture);
-		strTexture = texPath + L"LigthButton.png";
+		strTexture = texPath + L"LightMode.png";
 		app->RegisterTexture(L"LigthButton", strTexture);
 		strTexture = texPath + L"Rank.png";
 		app->RegisterTexture(L"Rank", strTexture);
+		strTexture = texPath + L"StatusMoji.png";
+		app->RegisterTexture(L"StatusMoji", strTexture);
+		strTexture = texPath + L"StatusMoji2.png";
+		app->RegisterTexture(L"StatusMoji2", strTexture);
+
 
 		strTexture = texPath + L"GameClear.jpg";
 		app->RegisterTexture(L"GameClear", strTexture);
@@ -168,7 +171,7 @@ namespace basecross {
 
             //自分自身にイベントを送る
             //これにより各ステージやオブジェクトがCreate時にシーンにアクセスできる
-            PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToStartStage");
+            PostEvent(0.0f, GetThis<ObjectInterface>(), GetThis<Scene>(), L"ToStatusStage");
         }
         catch (...) {
             throw;
