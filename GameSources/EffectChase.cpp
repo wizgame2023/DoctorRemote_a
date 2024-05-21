4cca1a19e1df137801d62aa779d1284348824817
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-21 09:29:56 +0900

    追従するエフェクトの更新

--- GameSources/EffectChase.cpp
+++ GameSources/EffectChase.cpp
@@ -1,6 +1,6 @@
 /*!
 @file effect.cpp
-@brief キャラクターなど実体
+@brief 追従するエフェクト
 担当　三瓶裕太
 */
 
@@ -8,4 +8,39 @@
 #include "Project.h"
 
 namespace basecross {
+	EffectChase::EffectChase(shared_ptr<Stage>& Stageptr, wstring TextureName, float maxTime, int addNumber,float lenght, Vec3 pushPos, Vec3 Scale) :
+		EffectPlayer(Stageptr, TextureName, maxTime, addNumber,lenght, pushPos, Scale)
+	{
+
+	}
+	EffectChase::~EffectChase()
+	{
+
+	}
+
+	void EffectChase::OnUpdate()
+	{
+
+		//親クラスのOnUpdate()を呼ぶ
+		MultiParticle::OnUpdate();
+
+		auto ptrTarget = GetStage()->GetSharedGameObject<Player>(L"GamePlayer");//GamePlayerというオブジェクトを取得
+		auto pos = ptrTarget->GetComponent<Transform>()->GetPosition();
+		m_ptrParticle->SetEmitterPos(pos);
+
+		float ElapsedTime = App::GetApp()->GetElapsedTime();//エルダータイム追加
+
+		for (auto ptrParticle : GetParticleVec())
+		{
+			for (auto& rParticleSprite : ptrParticle->GetParticleSpriteVec())
+			{
+				if (rParticleSprite.m_Active)
+				{
+					rParticleSprite.m_LocalPos += Vec3(0.0f, 0.0f, 0.0f) + (rParticleSprite.m_Velocity * ElapsedTime);//ここで追従しながらエフェクトの動きをする
+					rParticleSprite.m_LocalScale = m_Scale;//エフェクトのサイズを決める
+				}
+			}
+		}
+
+	}
 }
--- GameSources/EffectChase.h
+++ GameSources/EffectChase.h
@@ -1,5 +1,5 @@
 /*!
-@file Effect.h
+@file EffectCgase.h
 @brief ゲームステージ
 担当　三瓶裕太
 */
@@ -8,4 +8,17 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class EffectChase : public EffectPlayer
+	{
+	private:
+		//int m_addNumber;
+	public:
+		EffectChase(shared_ptr<Stage>& Stageptr, wstring TextureName, float maxTime, int addNumber,float lenght, Vec3 pushPos = Vec3(1.0f, 1.0f, 1.0f), Vec3 Scale = Vec3(1.0f, 1.0f, 1.0f));
+		~EffectChase() override;
+
+		void OnUpdate() override;
+		//void InsertEffect(const Vec3& Pos) override;
+
+	};
+
 }
