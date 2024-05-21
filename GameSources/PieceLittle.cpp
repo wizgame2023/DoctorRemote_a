831049293f4ef4cc8470cf8fbda991a9492c5c6d
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-21 14:09:13 +0900

    PieceLittleのファイルを追加

---
+++ GameSources/PieceLittle.cpp
@@ -0,0 +1,11 @@
+/*!
+@file BigPiece.cpp
+@brief ‘å‚«‚¢Œ‡•Ð‚ÌŽÀ‘•
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+}
+//end namespace basecross
---
+++ GameSources/PieceLittle.h
@@ -0,0 +1,12 @@
+/*!
+@file BigPiece.cpp
+@brief ‘å‚«‚¢Œ‡•Ğ‚ÌÀ‘•
+’S“–FˆíŒ©
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+}
+//end namespace basecross
