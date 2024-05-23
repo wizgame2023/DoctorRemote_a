2e2e64cb77bdd847c48033693d865393d03d0ef6
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-23 12:16:13 +0900

    EscapeManagerの更新

--- GameSources/EscapeManager.cpp
+++ GameSources/EscapeManager.cpp
@@ -8,9 +8,16 @@
 #include "Project.h"
 
 namespace basecross {
-	EscapeManager::EscapeManager(const shared_ptr<Stage>& stagePtr):
+	EscapeManager::EscapeManager(const shared_ptr<Stage>& stagePtr,Vec3 cameraEye,Vec3 cameraAt,Vec3 minRange,Vec3 maxRange,Vec3 playerStartPos,Vec3 TagetPos):
 		GameObject(stagePtr),
-		m_Time(0.0f)
+		m_Time(0.0f),
+		m_UpdateFlag(0),
+		m_CameraEye(cameraEye),
+		m_CameraAt(cameraAt),
+		m_MinRange(minRange),
+		m_MaxRange(maxRange),
+		m_PlayerStartPos(playerStartPos),
+		m_TargetPos(TagetPos)
 	{
 
 	}
@@ -29,10 +36,6 @@ namespace basecross {
 		m_Sprite->SetColor(Col4(0.0f, 0.0f, 0.0f, 0.0f));
 		m_SpriteCol = m_Sprite->GetColor();
 
-		//m_SpriteCol = Col4(1.0f, 1.0f, 1.0f, 0.0f);
-
-
-		m_UpdateFlag = 0;
 	}
 
 	void EscapeManager::Start()//開始
@@ -40,19 +43,20 @@ namespace basecross {
 		auto stage = GetStage();//ステージ取得
 
 		m_Camera = ObjectFactory::Create<Camera>();
-		m_Camera->SetEye(Vec3(-10.0f, 15.0f, -15.0f));
-		m_Camera->SetAt(Vec3(-10.0f, 0.0f, 0.0f));
+		m_Camera->SetEye(m_CameraEye);//メンバ変数
+		m_Camera->SetAt(m_CameraAt);//メンバ変数
 
 
 		auto View = stage->CreateView<SingleView>();
 		View->SetCamera(m_Camera);
 		stage->SetView(View);//ステージのビューを設定
 
-		//m_Player = stage->GetSharedGameObject<Player>(L"GamePlayer");//Playerを取得	
-		//m_PlayerPos = m_Player.lock()->GetComponent<Transform>()->GetPosition();//Positionを取得
+		//m_Player = stage->GetSharedGameObject<Player>(L"GamePlayer");//Playerを取得
+		Vec3 test = m_PlayerStartPos;
 
-		m_Player.lock()->GetComponent<Transform>()->SetPosition(-10.0f, 2.0f, 0.0f);
+		m_Player.lock()->GetComponent<Transform>()->SetPosition(m_PlayerStartPos);
 		m_Player.lock()->GetComponent<Transform>()->SetRotation(0.0f, 0.0f, 0.0f);
+		m_PlayerPos = m_Player.lock()->GetComponent<Transform>()->GetPosition();//Positionを取得
 
 		AABB CollisionActiveArea(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f));
 		stage->GetCollisionManager()->SetRootAABB(CollisionActiveArea);
@@ -66,12 +70,11 @@ namespace basecross {
 	{	
 		m_PlayerPos = m_Player.lock()->GetComponent<Transform>()->GetPosition();//Positionを取得
 
-		Vec3 minRange = Vec3(0.0f, 0.0f, 0.0f);//最小の範囲
-		Vec3 maxRange = Vec3(10.0f, 0.0f, 10.0f);//最大の範囲
-		if (m_PlayerPos.x <= maxRange.x && m_PlayerPos.x >= minRange.x)
+		//m_MinRange = Vec3(-20.0f, 0.0f, 3.0f);//最小の値 メンバ変数
+		//m_MaxRange = Vec3(-3.0f, 0.0f, 8.0f);//最大の値　メンバ変数
+		if (m_PlayerPos.x <= m_MaxRange.x && m_PlayerPos.x >= m_MinRange.x && m_UpdateFlag == 0)
 		{
-			int test = 0;
-			if (m_PlayerPos.z <= maxRange.x && m_PlayerPos.z >= minRange.z)
+			if (m_PlayerPos.z <= m_MaxRange.z && m_PlayerPos.z >= m_MinRange.z)
 			{
 				Start();
 				m_UpdateFlag = 1;//脱出する動作にフラグを変更
@@ -80,12 +83,23 @@ namespace basecross {
 	
 		if (m_UpdateFlag == 1)
 		{
-			Vec3 speed = Vec3(0.0f, 0.0f, 0.1f);
+			wstringstream wss;//デバック用文字列
+			wss << L"エスケープマネージャー：" << endl;
+
+
+			float speed = 5.0f;//速さ
+			float VecX = m_TargetPos.x - m_PlayerPos.x;//目標位置とPlayerとのX座標の距離を測っている
+			float VecZ = m_TargetPos.z - m_PlayerPos.z;//目標位置とPlayerとのZ座標の距離を測っている
+			float rad = atan2(VecZ, VecX);//角度を求める（ラジアン）
+
+			wss << "VecX:" << VecX << endl << "VecZ:" << VecZ << endl;//デバック文字列
+
 			auto& app = App::GetApp();
 			float delta = app->GetElapsedTime();//デルタタイムを取得
 			m_Time += delta;
-			m_PlayerPos = m_Player.lock()->GetComponent<Transform>()->GetPosition();
-			m_PlayerPos += speed;	
+			//m_PlayerPos = m_Player.lock()->GetComponent<Transform>()->GetPosition();
+			m_PlayerPos.x += (speed * cos(rad)) * delta;//間接的に距離を足している
+			m_PlayerPos.z += (speed * sin(rad)) * delta;//間接的に距離を足している
 
 			m_Player.lock()->GetComponent<Transform>()->SetPosition(m_PlayerPos);
 			if (m_Time >= 1.0f)
@@ -97,6 +111,10 @@ namespace basecross {
 					PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToScoreStage");
 				}
 			}
+			//デバック用文字列を生成
+			auto scene = app->GetScene<Scene>();
+			scene->SetDebugString(L"a\n" + wss.str());
+
 		}
 	}
 
--- GameSources/EscapeManager.h
+++ GameSources/EscapeManager.h
@@ -14,14 +14,20 @@ namespace basecross {
 	{
 	private:
 		shared_ptr<Camera> m_Camera;
+		Vec3 m_CameraEye;//脱出する際のカメラのポジション
+		Vec3 m_CameraAt;//脱出する際のカメラの注視点
 		shared_ptr<Sprite> m_Sprite;
 		weak_ptr<Player> m_Player;
-		Vec3 m_PlayerPos;
+		Vec3 m_PlayerPos;//Playerのポジション
+		Vec3 m_PlayerStartPos;//Startした際のPlayerのポジション
+		Vec3 m_MinRange;//最小の値
+		Vec3 m_MaxRange;//最大の値
+		Vec3 m_TargetPos;//到着地点
 		Col4 m_SpriteCol;//暗転するためのスプライトの色
 		int m_UpdateFlag;
 		float m_Time;
 	public:
-		EscapeManager(const shared_ptr<Stage>& stagePtr);//コンストラクタ
+		EscapeManager(const shared_ptr<Stage>& stagePtr, Vec3 cameraEye, Vec3 cameraAt, Vec3 minRange, Vec3 maxRange,Vec3 playerStart,Vec3 m_TargetPos);//コンストラクタ
 		~EscapeManager();//デストラクタ
 
 		void OnCreate() override;//生成
