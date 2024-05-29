412f2c4d40e833ddc783285083f23b82945e06eb
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-29 12:18:20 +0900

    コメントを文字数で表示できるようにした

--- GameSources/CommentManager.cpp
+++ GameSources/CommentManager.cpp
@@ -11,18 +11,49 @@ namespace basecross {
 	CommentManager::CommentManager(const shared_ptr<Stage>& stagePtr,const int moji):
 		GameObject(stagePtr),
 		m_mojiNum(moji),
+		m_addLine(0),
 		m_line(8),
-		m_column(13)
+		m_column(13),
+		m_mtime(0.2),
+		m_count(m_mtime * 13),
+		m_moji{13,13,13,13,13,13,13,13,13,13},
+		m_cnt(0)
 	{}
+	CommentManager::CommentManager(const shared_ptr<Stage>& stagePtr, const int moji,const int line) :
+		GameObject(stagePtr),
+		m_mojiNum(moji),//文字数
+		m_addLine(line),//何行目か
+		m_line(8),      //何行
+		m_column(13),   //何列
+		m_mtime(0.2),   //文字と文字の間を何行と仮定するか
+		m_count(m_mtime * 13),//次の行までの時間
+		m_moji{ 13,13,13,13,13,13,13,13,13,13 },
+		m_cnt(0)
+	{}
+
 
 	void CommentManager::OnCreate() {
 		m_mojiLine = m_mojiNum / m_column;
 		m_mojiColumn = m_mojiNum % m_column;
-		auto stage = GetStage();
 
 	}
 
 	void CommentManager::OnUpdate() {
+		float elapsed = App::GetApp()->GetElapsedTime();
+		auto stage = GetStage();
+		m_count -= elapsed;
+		
+		if (m_cnt <= m_mojiLine) {
+			if (m_count < 0) {
+				if (m_addLine > 0) {
+					m_moji[m_mojiLine] = m_mojiNum % (m_column * (m_cnt + 1));
+				}
+				m_com[m_cnt] = stage->AddGameObject<Comment>(m_moji[m_cnt], m_cnt+m_addLine);
 
+				m_count = m_mtime * m_column;
+				m_cnt++;
+				int a = 0;
+			}
+		}
 	}
 }
--- GameSources/CommentManager.h
+++ GameSources/CommentManager.h
@@ -11,8 +11,9 @@
 namespace basecross {
 	class CommentManager :public GameObject {
 	private:
-		int m_moji[5];
+		int m_moji[10];
 		int m_mojiNum;
+		int m_addLine;
 
 		int m_mojiLine;
 		int m_mojiColumn;
@@ -20,9 +21,18 @@ namespace basecross {
 		int m_line;
 		int m_column;
 
-		shared_ptr<Comment> m_com[5];
+		float m_count;
+		float m_mtime;
+		int m_cnt;
+		int m_lineNum;
+
+		bool m_lineFlag;
+
+		shared_ptr<Comment> m_com[10];
 	public:
 		CommentManager(const shared_ptr<Stage>& stagePtr,const int moji);
+		CommentManager(const shared_ptr<Stage>& stagePtr, const int moji,const int line);
+
 		virtual ~CommentManager(){}
 
 		virtual void OnCreate() override;
