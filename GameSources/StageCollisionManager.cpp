9ce775ce514b0971094923c1dc6ac934389545f9
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-29 16:08:57 +0900

    中身の追加

--- GameSources/StageCollisionManager.cpp
+++ GameSources/StageCollisionManager.cpp
@@ -1,11 +1,33 @@
 /*!
-@file TimeManager.cpp
+@file StageCollisionManager.cpp
 @brief ŠÔ‚Ì•\¦
-’S“–F
+’S“–FO•r—T‘¾
 */
 
 #include "stdafx.h"
 #include "Project.h"
 
 namespace basecross {
+	StageCollisionManager::StageCollisionManager(shared_ptr<Stage>& stagePtr) :
+		GameObject(stagePtr),
+		m_CollisionSwhich(true)
+	{
+
+	}
+	StageCollisionManager::~StageCollisionManager()
+	{
+
+	}
+
+	void StageCollisionManager::OnCreate()
+	{
+
+	}
+
+	void StageCollisionManager::OnUpdate()
+	{
+
+	}
+	
+
 }
--- GameSources/StageCollisionManager.h
+++ GameSources/StageCollisionManager.h
@@ -1,11 +1,23 @@
 /*!
-@file TimeManager.h
+@file StageCollisionManager.h
 @brief ŠÔ‚Ì•\¦
-’S“–F
+’S“–FO•r—T‘¾
 */
 
 #pragma once
 #include "stdafx.h"
 
 namespace basecross {
+	class StageCollisionManager :public GameObject
+	{
+	private:
+		bool m_CollisionSwhich;
+	public:
+		StageCollisionManager(shared_ptr<Stage>& stagePtr);
+		~StageCollisionManager();
+
+		void OnCreate()override;
+		void OnUpdate()override;
+
+	};
 }
