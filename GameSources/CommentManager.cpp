05f18e6f8c4a0015965288171102f6c8f8456260
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-29 09:56:28 +0900

    名前の修正

--- GameSources/CommentManager.cpp
+++ GameSources/CommentManager.cpp
@@ -8,4 +8,21 @@
 #include "Project.h"
 
 namespace basecross {
+	CommentManager::CommentManager(const shared_ptr<Stage>& stagePtr,const int moji):
+		GameObject(stagePtr),
+		m_mojiNum(moji),
+		m_line(8),
+		m_column(13)
+	{}
+
+	void CommentManager::OnCreate() {
+		m_mojiLine = m_mojiNum / m_column;
+		m_mojiColumn = m_mojiNum % m_column;
+		auto stage = GetStage();
+
+	}
+
+	void CommentManager::OnUpdate() {
+
+	}
 }
