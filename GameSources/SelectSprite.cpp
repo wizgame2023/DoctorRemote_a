a5f6dbcaed624a8a21546f2ffccb49dceb5555c1
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-30 02:07:32 +0900

    タイトルで画像を動かせる処理の途中

--- GameSources/SelectSprite.cpp
+++ GameSources/SelectSprite.cpp
@@ -1,6 +1,6 @@
 /*!
-@file Sprite.cpp
-@brief スプライト
+@file SelectSprite.cpp
+@brief 動かすことができるスプライト
 担当：逸見
 */
 
@@ -8,4 +8,37 @@
 #include "Project.h"
 
 namespace basecross {
+	SelectSprite::SelectSprite(const shared_ptr<Stage>& stagePtr):
+		GameObject(stagePtr),
+		m_height(100),
+		m_heightMax(325)
+	{}
+
+	void SelectSprite::OnCreate() {
+		auto stage = GetStage();
+		auto sprite = stage->AddGameObject<Sprite>(170, 70, L"White", Vec3(0.0f, -100.0f, 0.0f));
+		m_trans = sprite->GetComponent<Transform>();
+
+		auto color = Col4(0.0f, 0.0f, 0, 1.0f);
+		sprite->SetColor(color);
+
+	}
+	void SelectSprite::OnUpdate() {
+		auto stage = GetStage();
+		float elapsed = App::GetApp()->GetElapsedTime();
+		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
+
+		if (cntlVec[0].fThumbLY > -0.9f) {
+			if (m_moveCheck) return;
+			if (m_heightMax > m_height && !m_checkR) {
+				m_height -= 50;
+				m_trans->SetPosition(0.0f,m_height, 0.0f);
+				m_checkR = true;
+			}
+		}
+		if (cntlVec[0].fThumbLY < -0.9f && m_checkR == true) {
+			if (m_moveCheck) return;
+			m_checkR = false;
+		}
+	}
 }
--- GameSources/SelectSprite.h
+++ GameSources/SelectSprite.h
@@ -1,6 +1,6 @@
 /*!
-@file Sprite.h
-@brief スプライト
+@file SelectSprite.h
+@brief 動かすことができるスプライト
 担当：逸見
 */
 
@@ -8,4 +8,24 @@
 #include "stdafx.h"
 
 namespace basecross {
+	class SelectSprite :public GameObject {
+	private:
+		float m_width;
+		float m_widthMax;
+		float m_height;
+		float m_heightMax;
+
+		bool m_checkR;
+		bool m_checkL;
+		bool m_moveCheck;
+
+		shared_ptr<Transform> m_trans;
+
+	public:
+		SelectSprite(const shared_ptr<Stage>& stagePtr);
+		virtual ~SelectSprite(){}
+
+		virtual void OnCreate();
+		virtual void OnUpdate();
+	};
 }
