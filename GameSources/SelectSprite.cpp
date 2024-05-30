84cd8ba43b52e790d9012323a7f46d846bb0f211
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-30 11:21:00 +0900

    タイトルでステージを選べる

--- GameSources/SelectSprite.cpp
+++ GameSources/SelectSprite.cpp
@@ -8,19 +8,34 @@
 #include "Project.h"
 
 namespace basecross {
+
+	enum STAGE {
+		TUTORIAL,
+		STAGE1,
+		STAGE2,
+		STAGE3
+	};
+
 	SelectSprite::SelectSprite(const shared_ptr<Stage>& stagePtr):
 		GameObject(stagePtr),
-		m_height(100),
-		m_heightMax(325)
+		m_height(-100),
+		m_heightMax(-100),
+		m_heightMin(-325),
+		m_count(10.0f),
+		m_checkD(false),
+		m_checkU(false),
+		m_moveCheck(false),
+		m_blinkCheck(false),
+		m_color(1.0f,1.0f,1.0f,1.0f)
 	{}
 
 	void SelectSprite::OnCreate() {
 		auto stage = GetStage();
-		auto sprite = stage->AddGameObject<Sprite>(170, 70, L"White", Vec3(0.0f, -100.0f, 0.0f));
-		m_trans = sprite->GetComponent<Transform>();
-
+		m_sprite = stage->AddGameObject<Sprite>(170, 70, L"White", Vec3(0.0f, -100.0f, 0.0f),-1);
+		m_trans = m_sprite->GetComponent<Transform>();
+		m_trans->SetPosition(Vec3(0, -100, 0));
 		auto color = Col4(0.0f, 0.0f, 0, 1.0f);
-		sprite->SetColor(color);
+		m_sprite->SetColor(color);
 
 	}
 	void SelectSprite::OnUpdate() {
@@ -28,17 +43,96 @@ namespace basecross {
 		float elapsed = App::GetApp()->GetElapsedTime();
 		auto cntlVec = App::GetApp()->GetInputDevice().GetControlerVec();
 
-		if (cntlVec[0].fThumbLY > -0.9f) {
+		//ステージを選ぶ
+		if (cntlVec[0].fThumbLY < -0.9f) {
 			if (m_moveCheck) return;
-			if (m_heightMax > m_height && !m_checkR) {
-				m_height -= 50;
+			if (m_heightMin < m_height && !m_checkD) {
+				m_height -= 75;
 				m_trans->SetPosition(0.0f,m_height, 0.0f);
-				m_checkR = true;
+				m_checkD = true;
 			}
 		}
-		if (cntlVec[0].fThumbLY < -0.9f && m_checkR == true) {
+		if (cntlVec[0].fThumbLY > -0.9f && m_checkD == true) {
 			if (m_moveCheck) return;
-			m_checkR = false;
+			m_checkD = false;
 		}
+
+		if (cntlVec[0].fThumbLY > 0.9) {
+			if (m_moveCheck) return;
+			if (m_heightMax > m_height && !m_checkU) {
+				m_height += 75;
+				m_trans->SetPosition(0.0f, m_height, 0.0f);
+				m_checkU = true;
+			}
+		}
+		if (cntlVec[0].fThumbLY < 0.9 && m_checkU == true) {
+			if (m_moveCheck) return;
+			m_checkU = false;
+		}
+
+		//Bボタンで決定
+		if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_B) {
+			m_moveCheck = true;
+			m_blinkCheck = true;
+		}
+
+		if (m_height == m_heightMax) {
+			m_stage = TUTORIAL;
+		}
+		else if (m_height == m_heightMax-75) {
+			m_stage = STAGE1;
+		}
+		else if (m_height == m_heightMax - 75 * 2) {
+			m_stage = STAGE2;
+		}
+		else if (m_height == m_heightMax - 75 * 3) {
+			m_stage = STAGE3;
+		}
+
+		//決定したら点滅
+		if (m_blinkCheck) {
+			if ((int)m_count % 2 == 0) {
+				m_sprite->SetColor(Col4(0, 0, 0, 0));
+			}
+			else if ((int)m_count % 2 == 1) {
+				m_sprite->SetColor(Col4(0, 0, 0, 1));
+			}
+			m_count -= elapsed * 10.0f;
+		}
+		if (m_count < 0) {
+			switch (m_stage)
+			{
+			case 0:
+				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToClearStage");
+				break;
+			case 1:
+				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage");
+				break;
+			case 2:
+				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToStatusStage");
+				break;
+			case 3:
+				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameOverStage");
+				break;
+			default:
+				break;
+			}
+		}
+
+		//wstringstream wss(L"");
+		//auto scene = App::GetApp()->GetScene<Scene>();
+		//auto gameStage = scene->GetGameStage();
+		//wss <<L"stage : "
+		//	<<m_stage
+		//	<<L"\nheight : "
+		//	<<m_height
+		//	<<L"\ncount"
+		//	<<m_count
+		//	<<L"\nBlink"
+		//	<<m_blinkCheck
+		//	<< endl;
+		//scene->SetDebugString(wss.str());
+
 	}
+
 }
--- GameSources/SelectSprite.h
+++ GameSources/SelectSprite.h
@@ -6,20 +6,28 @@
 
 #pragma once
 #include "stdafx.h"
+#include "Sprite.h"
 
 namespace basecross {
 	class SelectSprite :public GameObject {
 	private:
+		int m_stage;
+
 		float m_width;
 		float m_widthMax;
 		float m_height;
 		float m_heightMax;
+		float m_heightMin;
+		float m_count;
 
-		bool m_checkR;
-		bool m_checkL;
+		bool m_checkD;
+		bool m_checkU;
 		bool m_moveCheck;
+		bool m_blinkCheck;
 
 		shared_ptr<Transform> m_trans;
+		shared_ptr<Sprite> m_sprite;
+		Col4 m_color;
 
 	public:
 		SelectSprite(const shared_ptr<Stage>& stagePtr);
