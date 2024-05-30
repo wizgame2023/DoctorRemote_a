169d10a19b98626d4e78b6e1e677ec2a707364c1
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-30 12:34:02 +0900

    StageCollisionManagerの基本的な動作を作成

--- GameSources/StageCollisionManager.cpp
+++ GameSources/StageCollisionManager.cpp
@@ -26,8 +26,31 @@ namespace basecross {
 
 	void StageCollisionManager::OnUpdate()
 	{
+		CollisionActive(true);
+	}
+
+	void StageCollisionManager::CollisionActive(bool On)
+	{
+		auto stage = GetStage();
+		auto ptrPlayer = stage->GetSharedGameObject<Player>(L"GamePlayer");
+		auto PlayerPos = ptrPlayer->GetComponent<Transform>()->GetPosition();
+
+		if (On == true)
+		{
+			AABB CollisionActiveArea = AABB(PlayerPos + Vec3(-50.0f, -50.0f, -50.0f), PlayerPos + Vec3(50.0f, 50.0f, 50.0f));
+			stage->GetCollisionManager()->SetRootAABB(CollisionActiveArea);
+		}
+		if (On == false)
+		{
+			AABB CollisionActiveArea = AABB(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f));
+			stage->GetCollisionManager()->SetRootAABB(CollisionActiveArea);
+		}
 
 	}
-	
+
+	void StageCollisionManager::SetCollisionSwhich(bool On)
+	{
+		m_CollisionSwhich = On;
+	}
 
 }
--- GameSources/StageCollisionManager.h
+++ GameSources/StageCollisionManager.h
@@ -11,13 +11,17 @@ namespace basecross {
 	class StageCollisionManager :public GameObject
 	{
 	private:
-		bool m_CollisionSwhich;
+		bool m_CollisionSwhich;	
+
+		void CollisionActive(bool On);//ステージ上の有効なコリジョン範囲を決める
+
 	public:
 		StageCollisionManager(shared_ptr<Stage>& stagePtr);
 		~StageCollisionManager();
 
 		void OnCreate()override;
 		void OnUpdate()override;
+		void SetCollisionSwhich(bool On);//コリジョン範囲を決めるためのフラグを取得する関数
 
 	};
 }
