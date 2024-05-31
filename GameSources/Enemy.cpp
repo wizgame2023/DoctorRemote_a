f803934c5d54dfa57f2bc8e980bf263dbf1345e9
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-05-31 16:10:38 +0900

    使わない処理をコメントアウト

--- GameSources/Enemy.cpp
+++ GameSources/Enemy.cpp
@@ -99,7 +99,7 @@ namespace basecross {
 			int a = 0;
 			GetStage()->GetSharedGameObject<StageManager>(L"StageManager")->SetStageFlag(2);//ステージの全体フラグ進行
 			GetStage()->RemoveGameObject<Enemy>(GetThis<Enemy>());
-			PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToScoreStage");
+			//PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToScoreStage");
 		}
 
 	}
