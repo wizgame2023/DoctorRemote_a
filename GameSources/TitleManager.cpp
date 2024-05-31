7a6a96220a47a33b3b314089e2a3e195bb137029
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-05-31 16:42:20 +0900

    オブジェクトを配置

--- GameSources/TitleManager.cpp
+++ GameSources/TitleManager.cpp
@@ -13,10 +13,44 @@ namespace basecross {
 	{}
 
 	void TitleManager::OnCreate() {
+		auto trans = GetComponent<Transform>();
+
+		auto deg = -90;
+		auto deg2 = -45;
+		auto rad = XMConvertToRadians(deg);
+		auto rad2 = XMConvertToRadians(deg2);
+
+		trans->SetPosition(Vec3(-2.0f,0.0f,-2.0f));
+		trans->SetRotation(Vec3(0.0f, rad, rad2));
+
+		Mat4x4 spanMat;
+		spanMat.affineTransformation(
+			Vec3(0.5f, 0.5f, 0.5f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0f, 0.0f)
+		);
+
+		auto ptrDraw = AddComponent<PNTBoneModelDraw>();
+		ptrDraw->SetMultiMeshResource(L"Sensuikan_Mesh");
+		//ptrDraw->SetSamplerState(SamplerState::LinearWrap);
+		ptrDraw->SetMeshToTransformMatrix(spanMat);
+
+		ptrDraw->SetOwnShadowActive(true);
+		ptrDraw->AddAnimation(L"Default", 0, 90, true, 30.0f);
+		ptrDraw->ChangeCurrentAnimation(L"Default");
+
+		//‰e‚ð‚Â‚¯‚é
+		//auto ptrShadow = AddComponent<Shadowmap>();
+		//ptrShadow->SetMultiMeshResource(L"Sensuikan_Mesh");
+		//ptrShadow->SetMeshToTransformMatrix(spanMat);
 
 	}
 
 	void TitleManager::OnUpdate() {
+		auto elapsedTime = App::GetApp()->GetElapsedTime();
+		auto drawComp = GetComponent<PNTBoneModelDraw>();
+		drawComp->UpdateAnimation(elapsedTime);
 
 	}
 }
