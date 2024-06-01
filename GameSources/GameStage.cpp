446c670aeb2107e9709aed49567ba890f152509a
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-06-01 22:24:42 +0900

    新しいマネージャーの追加

--- GameSources/GameStage.cpp
+++ GameSources/GameStage.cpp
@@ -406,7 +406,8 @@ namespace basecross {
 			CreatePlayer();
 			//敵のかけらを表示
 			CreateEnemyPiece();
-			CreateEnemyPiece2();//ランダムにかけらが出るようになる
+			//CreateEnemyPiece2();//ランダムにかけらが出るようになる
+			AddGameObject<RandCreateManager>(L"kakeraMapDateLevels.csv", 150);//ランダムにかけらが出るようになる
 			CerateBreakEnemyPiece();
 			//CreateRecoveryWall();//治す壁を生成 現在没データ化
 			AddGameObject<Ground>();//地面を生成
