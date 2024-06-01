42154eaf946ec729255a8944350cf1b7833c939b
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-06-01 17:23:13 +0900

    ミニマップに映るBigPieceの処理

--- GameSources/MiniMapBigPiece.h
+++ GameSources/MiniMapBigPiece.h
@@ -1,11 +1,26 @@
 /*!
 @file PieceGarge.h
-@brief 敵の欠片のゲージ
-担当：
+@brief ミニマップにビックピースの位置を表示
+担当：三瓶裕太
 */
 
 #pragma once
 #include "stdafx.h"
+#include "BigPiece.h"
+
 
 namespace basecross {
+	class MiniMapBigPiece :public Sprite
+	{
+	private:
+		wstring m_ObjName;
+		bool m_Existence;//対応するオブジェクトがあるかを表す
+	public:
+		MiniMapBigPiece(const shared_ptr<Stage>& stagePtr, float width, float height, wstring meshResName, Vec3 pos, int layer,wstring objName);
+		~MiniMapBigPiece()override;
+
+		void OnCreate()override;
+		void OnUpdate()override;
+		void SetExistence(bool Existence);
+	};
 }
