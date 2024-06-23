a269302e5a454ec70289ebe7bf60b78ea36634d1
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-06-23 11:29:53 +0900

    デフォルト引数追加

--- GameSources/Garge.h
+++ GameSources/Garge.h
@@ -47,7 +47,7 @@ namespace basecross {
 		Garge(const shared_ptr<Stage>& stagePtr,
 			const float sizeX, const float sizeY, wstring meshResName, //画像のサイズ
 			float ratio, float width, float heigth, //ゲージの割合と表示するサイズ
-			Col4 color, Col4 color2, Vec3 screen, Vec3 distance);
+			Col4 color, Col4 color2, Vec3 screen, Vec3 distance = Vec3(0.0));
 		virtual ~Garge(){}
 
 		virtual void OnCreate() override;
