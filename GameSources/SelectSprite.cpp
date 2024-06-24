f57d92bb36f4af8b3b26e592ceed38ebc9ee55a8
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-06-24 23:48:59 +0900

    消すことができる関数を追加、その他修正

--- GameSources/SelectSprite.cpp
+++ GameSources/SelectSprite.cpp
@@ -11,15 +11,14 @@ namespace basecross {
 
 	enum STAGE {
 		TUTORIAL,
-		STAGE1,
-		STAGE2,
-		STAGE3
+		STAGESELECT,
+		EXIT,
 	};
 
 	SelectSprite::SelectSprite(const shared_ptr<Stage>& stagePtr):
 		GameObject(stagePtr),
 		m_height(-100),
-		m_heightMax(-100),
+		m_heightMax(-175),
 		m_heightMin(-325),
 		m_spaces(75.0f),
 		m_count(10.0f),
@@ -27,14 +26,15 @@ namespace basecross {
 		m_checkU(false),
 		m_moveCheck(false),
 		m_blinkCheck(false),
+		m_selectStageFlag(false),
 		m_color(1.0f,1.0f,1.0f,1.0f)
 	{}
 
 	void SelectSprite::OnCreate() {
 		auto stage = GetStage();
-		m_sprite = stage->AddGameObject<Sprite>(170, 70, L"SelectWhite", Vec3(0.0f, -100.0f, 0.0f),-1);
+		m_sprite = stage->AddGameObject<Sprite>(170, 70, L"SelectWhite", Vec3(0.0f, -175.0f, 0.0f),-1);
 		m_trans = m_sprite->GetComponent<Transform>();
-		m_trans->SetPosition(Vec3(0, -100, 0));
+		m_trans->SetPosition(Vec3(0, -175, 0));
 		m_color = Col4(1.0f, 1.0f, 1.0f, 0.5f);
 		m_sprite->SetColor(m_color);
 
@@ -73,21 +73,33 @@ namespace basecross {
 
 		//Bƒ{ƒ^ƒ“‚ÅŒˆ’è
 		if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_B) {
-			m_moveCheck = true;
+			if (m_stage == TUTORIAL && m_stage == EXIT) {
+				m_moveCheck = true;
+			}
+			if (m_stage == STAGESELECT) {
+				m_count = 0;
+			}
 			m_blinkCheck = true;
+		}
+		if (cntlVec[0].wPressedButtons & XINPUT_GAMEPAD_A) {
+			if (m_selectStageFlag) {
+				m_selectStage->ThisDestroy();
+				m_stageFrame->ThisDestory();
+				m_sprite->SetColor(Col4(1.0f, 1.0f, 1.0, 0.5f));
+				m_count = 10.0f;
+				m_selectStageFlag = false;
+				m_blinkCheck = false;
+			}
 		}
 
 		if (m_height == m_heightMax) {
 			m_stage = TUTORIAL;
 		}
 		else if (m_height == m_heightMax-m_spaces) {
-			m_stage = STAGE1;
+			m_stage = STAGESELECT;
 		}
 		else if (m_height == m_heightMax - m_spaces * 2) {
-			m_stage = STAGE2;
-		}
-		else if (m_height == m_heightMax - m_spaces * 3) {
-			m_stage = STAGE3;
+			m_stage = EXIT;
 		}
 
 		//Œˆ’è‚µ‚½‚ç“_–Å
@@ -104,19 +116,34 @@ namespace basecross {
 			switch (m_stage)
 			{
 			case 0:
-				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToTutorialStage");
+				if (!m_selectStageFlag) {
+					PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToTutorialStage");
+				}
 				break;
 			case 1:
-				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage");
+				if (!m_selectStageFlag) {
+					m_stageFrame = stage->AddGameObject<Sprite>(600, 300, L"Score", Vec3(0.0f));
+					m_selectStage = GetStage()->AddGameObject<StageSelectSprite>();
+					m_selectStage->SetLimitNum(10);
+					m_selectStageFlag = true;
+				}
 				break;
 			case 2:
-				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage2");
-				break;
-			case 3:
-				PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage3");
+				if (!m_selectStageFlag) {
+					PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage10");
+				}
 				break;
 			default:
 				break;
+			}
+
+			if (m_selectStageFlag) {
+				if (m_selectStage->GetBlinkTime() <= 0) {
+					int stage = m_selectStage->GetNum();
+					wstring stageNum = to_wstring(stage);
+					PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage" + stageNum);
+				}
+
 			}
 		}
 
@@ -138,5 +165,8 @@ namespace basecross {
 	bool SelectSprite::GetMoveCheck()
 	{
 		return m_moveCheck;
+	}
+	void SelectSprite::StageMove(wstring stage) {
+		PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStage"+stage);
 	}
 }
