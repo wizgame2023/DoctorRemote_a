2e8bd43bb268bf805ad9bd5bf3c1bf149412d675
Chinami Ando <zeal23420147@fsgcl.jp>
2024-06-25 09:57:47 +0900

    新しいパスを追加

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -63,6 +63,10 @@ namespace basecross {
 		app->RegisterTexture(L"TimeWaku", strTexture);
 		strTexture = texPath + L"Map_sWaku.png";
 		app->RegisterTexture(L"Map_sWaku", strTexture);
+		strTexture = texPath + L"DecisionButton.png";
+		app->RegisterTexture(L"DecisionButton", strTexture);
+		strTexture = texPath + L"RetrunButton.png";
+		app->RegisterTexture(L"RetrunButton", strTexture);
 
 		//背景など1280x800//////////////////////////////////////////////////////////////
 		strTexture = texPath + L"Clear.png";
