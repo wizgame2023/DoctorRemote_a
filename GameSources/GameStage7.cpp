7b491f8b3909a07a028e39986f489ccc6b74fd06
Yushin Okawara <zeal23420135@fsgcl.jp>
2024-06-27 11:33:56 +0900

    欠片の取得率の修正・ランダムの欠片を増量

--- GameSources/GameStage7.cpp
+++ GameSources/GameStage7.cpp
@@ -401,7 +401,7 @@ namespace basecross {
 			//敵のかけらを表示
 			CreateEnemyPiece();
 			//CreateEnemyPiece2();//ランダムにかけらが出るようになる
-			AddGameObject<RandCreateManager>(L"kakeraMapLevel7.csv", 150, 30);//ランダムにかけらが出るようになる
+			AddGameObject<RandCreateManager>(L"kakeraMapLevel7.csv", 150, 45);//ランダムにかけらが出るようになる
 
 			CerateBreakEnemyPiece();
 			//CreateRecoveryWall();//治す壁を生成 現在没データ化
