60a91d923e759a7ed53051098207359b58587157
Hemmi Akari <zeal23300133@fsgcl.jp>
2024-06-28 09:29:08 +0900

    チャージ音の修正

--- GameSources/Player.cpp
+++ GameSources/Player.cpp
@@ -128,21 +128,21 @@ namespace basecross {
 				auto soundE = App::GetApp()->GetXAudio2Manager();
 
 				if (cntlVec[0].wButtons & XINPUT_GAMEPAD_B) {
-					if (m_bulletTime <= m_bulletChargeTime*3) {
+					if (m_bulletTime <= m_bulletChargeTime * 3) {
 						m_bulletTime += elapsedTime;
-						if (m_bulletTime >= 1) {
+						if (m_bulletTime >= m_bulletChargeTime) {
 							if (!m_chargeBulletSE[0]) {
 								soundE->Start(L"Charge1", 0, 0.5f);
 								m_chargeBulletSE[0] = true;
 							}
 						}
-						if (m_bulletTime >= 2) {
+						if (m_bulletTime >= m_bulletChargeTime * 2) {
 							if (!m_chargeBulletSE[1]) {
 								soundE->Start(L"Charge2", 0, 0.5f);
 								m_chargeBulletSE[1] = true;
 							}
 						}
-						if (m_bulletTime >= 3) {
+						if (m_bulletTime >= m_bulletChargeTime * 3) {
 							if (!m_chargeBulletSE[2]) {
 								soundE->Start(L"Charge3", 0, 0.5f);
 								m_chargeBulletSE[2] = true;
@@ -152,7 +152,7 @@ namespace basecross {
 
 					}
 					else {
-						m_bulletTime = m_bulletChargeTime*3;
+						m_bulletTime = m_bulletChargeTime * 3;
 					}
 					m_bulletRatio = m_bulletTime / (m_bulletChargeTime * 3);
 				}
@@ -161,12 +161,12 @@ namespace basecross {
 				}
 
 				if (cntlVec[0].wReleasedButtons & XINPUT_GAMEPAD_B) {
-					if (m_bulletTime >= m_bulletChargeTime*3) {
+					if (m_bulletTime >= m_bulletChargeTime * 3) {
 						auto bullet = stage->AddGameObject<Bullet>(Vec3(ptrPos.x, ptrPos.y - 0.3f, ptrPos.z), Vec3(0.7f), 50.0f, frontAngle, 20.0f);
 						bullet->SetBulletLevel(3);
 						BulletSE();
 					}
-					else if (m_bulletTime >= m_bulletChargeTime*2) {
+					else if (m_bulletTime >= m_bulletChargeTime * 2) {
 						auto bullet = stage->AddGameObject<Bullet>(Vec3(ptrPos.x, ptrPos.y - 0.3f, ptrPos.z), Vec3(0.5f), 45.0f, frontAngle, 12.0f);
 						bullet->SetBulletLevel(2);
 						BulletSE();
