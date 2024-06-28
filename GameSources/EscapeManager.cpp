5dd8c367e7137efba5fa8181955707eb342ecb8b
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-06-28 13:56:04 +0900

    ステージが終了する時間を変更

--- GameSources/EscapeManager.cpp
+++ GameSources/EscapeManager.cpp
@@ -200,7 +200,7 @@ namespace basecross {
 				{
 					GetStage()->AddGameObject<Sprite>(1280, 800, L"Clear", Vec3(), 0);//帯を出す
 					m_endTime += delta;
-					if (m_endTime > 0.5f)
+					if (m_endTime > 1.0f)
 					{
 						PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToScoreStage");//スコアステージに進む
 					}
