09d2ea884ff219100828daaff3c108b697ecedd1
Chinami Ando <zeal23420147@fsgcl.jp>
2024-07-16 09:44:28 +0900

    ブロックのコリジョンを見えなくするように変更

--- GameSources/Block.cpp
+++ GameSources/Block.cpp
@@ -40,7 +40,7 @@ namespace basecross {
 		ptrColl->SetFixed(true);	
 		ptrColl->SetSleepActive(true);//ぶつからない限りスリープ状態になる
 
-		ptrColl->SetDrawActive(true);//コリジョンを見えるようにする
+		ptrColl->SetDrawActive(false);//コリジョンを見えるようにする
 
 
 		GetStage()->SetCollisionPerformanceActive(true);
