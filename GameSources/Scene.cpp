7b0d743e3e3f1c3d44ebdecc02d543a59ebc354b
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2024-07-24 11:00:49 +0900

    ミスを修正

--- GameSources/Scene.cpp
+++ GameSources/Scene.cpp
@@ -565,7 +565,7 @@ namespace basecross {
 	void Scene::OnEvent(const shared_ptr<Event>& event) {
 		if (event->m_MsgStr == L"ToGameStage1") {
 			//ゲームステージの設定
-			ResetActiveStage<GameStage10>();
+			ResetActiveStage<GameStage1>();
 		}
 		else if (event->m_MsgStr == L"ToGameStage2") {
 			//ゲームステージの設定
